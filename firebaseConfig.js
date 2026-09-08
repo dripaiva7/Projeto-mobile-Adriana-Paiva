b6c5c757f0c2f84feb1d889b88567bc0838c040f
unknown <dri.paiva7@gmail.com>
2026-09-08 18:53:00 -0300

    atualiza versão sdk e adiciona configuração firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbkJ497R4tiUnFMzQX0DwC7qocD3K_4ys",
+  authDomain: "mdb-app-4e714.firebaseapp.com",
+  projectId: "mdb-app-4e714",
+  storageBucket: "mdb-app-4e714.firebasestorage.app",
+  messagingSenderId: "351686712362",
+  appId: "1:351686712362:web:924b769cec700f54377753"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
